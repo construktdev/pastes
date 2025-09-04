3fe374058df53ddf883b2d52cc607128ac0dff1f
Conrad Richter <construktai@gmail.com>
2025-09-04 19:21:11 +0200

    Update bashrcfun.sh

--- bashrcfun.sh
+++ bashrcfun.sh
@@ -1,7 +1,7 @@
 ## ====== NUR EMOJIS MIT FARBE ======
 
 # Emojis definieren
-EMOJIS=("🐱" "😈" "💩" "🤯" "🚀" "🍕" "😂" "😜")
+EMOJIS=("🤯" "🚀" "🍕" "😂")
 
 # Zufälliges Emoji mit zufälliger Farbe
 PS1='\[$(tput setaf $((RANDOM % 7 + 1)))\]${EMOJIS[$RANDOM % ${#EMOJIS[@]}]} \$ \[$(tput sgr0)\]'
