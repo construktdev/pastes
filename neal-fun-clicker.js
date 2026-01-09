d681755c78a45f2f466f6ac1f76ed8b15582341e
Conrad Richter <construktai@gmail.com>
2026-01-09 09:52:09 +0100

    Update neal-fun-clicker.js with usage instructions
    Added instructions for console usage and button ID requirement.

---
+++ neal-fun-clicker.js
@@ -0,0 +1,5 @@
+// Paste this in the console tab
+// IMPORTANT: 
+// You have to give the button the id="button" attribute
+// Otherwise this won't work
+setInterval(() => {document.getElementById("button").click()}, 1);
